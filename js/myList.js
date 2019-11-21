49ce517d40e6142a68e67c3118eb2d07c3ba856b
Sneha Ratakonda <A00210429@mycambrian.ca>
2019-11-21 15:37:57 -0500

    first commit

---
+++ js/myList.js
@@ -0,0 +1,70 @@
+function addNewList()
+{
+
+    event.preventDefault();
+
+    const todo = document.querySelector('#listText');
+    const value = todo.value.trim();
+
+    if (value.length > 0) {
+        const hash = (Date.now().toString(36).substr(2, 4) + performance.now().toString(36).replace('.','').substr(0, 4) + Math.random().toString(36).substr(3, 4)).toUpperCase();
+
+        const id = `todo-select-${hash}`;
+        const template = document.querySelector('#item-lists');
+        const item = document.importNode(template.content, true);
+        const label = item.querySelector('label[for]');
+        const input = item.querySelector('#todo-checkbox')
+        const list = document.querySelector('#todo-myList');
+
+        input.setAttribute('id', id);
+        label.setAttribute('for', id);
+
+        label.textContent = value;
+
+        const database = window.localStorage;
+        const itemString = new XMLSerializer().serializeToString(item);
+        database.setItem(id, itemString);
+
+
+        list.appendChild(item);
+    }
+
+    todo.value = '';
+
+}
+
+function checkBoxUpdate(checkbox) {
+   
+    if (checkbox.checked) {
+        checkbox.setAttribute('checked', checkbox.checked);
+       
+       
+       
+    } else {
+        checkbox.removeAttribute('checked');
+      
+    }
+    const item = new XMLSerializer().serializeToString(checkbox.parentNode);
+    const checkBoxid = checkbox.id;
+    window.localStorage.setItem(checkBoxid, item);
+}
+
+function bindAddEventListener() 
+{
+    let btn = document.querySelector('#add-img');
+    btn.addEventListener('click', addNewList);
+    linkToDos();
+}
+
+function linkToDos(){
+    const dataObj = window.localStorage;
+    Object.keys(dataObj).forEach(addStoredItems);
+
+}
+function addStoredItems(item) {
+    const db = window.localStorage;
+    const list = document.querySelector('#todo-myList');
+    const node = document.createRange().createContextualFragment(db.getItem(item));
+    list.appendChild(node);
+}
+bindAddEventListener();
